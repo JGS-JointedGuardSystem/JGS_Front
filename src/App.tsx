faea896b76d991f5cfcd9f35c2586badf710315f
inung1004 <dksdbswl10430@naver.com>
2023-04-26 12:29:51 +0900

    💡 (#3) :: recoil, react-query 사용 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import MainRouter from "./router/MainRouter";
+import { globalStyle } from "./styles/GlobalStyle";
+import { Global, ThemeProvider } from "@emotion/react";
+import { RecoilRoot } from "recoil";
+import { QueryClientProvider, QueryClient } from "react-query";
 
-const App: React.FC = () => {
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      staleTime: 1000,
+      refetchInterval: 0,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+function App() {
   return (
-    <div className="App">
-      <h3>Hello Typescript</h3>
-    </div>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Global styles={globalStyle} />
+        <BrowserRouter>
+          <MainRouter />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </RecoilRoot>
   );
-};
+}
 
 export default App;
