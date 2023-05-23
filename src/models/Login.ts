1b6d5048cff81e0786e2e746a1d0747ccf75ad8f
inung1004 <dksdbswl10430@naver.com>
2023-05-23 22:22:39 +0900

    👀 :: (#9) 로그인 인터페이스 수정

---
+++ src/models/Login.ts
@@ -0,0 +1,15 @@
+export interface ContainerProps {
+  bgImage: string;
+}
+
+export interface LoginInputDataType {
+  title: string;
+  name: string;
+  placeholder: string;
+  type: string;
+}
+
+export interface LoginInputType {
+  id: string;
+  password: string;
+}
