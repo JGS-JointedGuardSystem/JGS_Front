7efba7eb129117a618bddc709fa0a8910babcccc
inung1004 <dksdbswl10430@naver.com>
2023-05-23 22:27:09 +0900

    👀 :: (#9) 로그인 컴포넌트 분리

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,20 +1,73 @@
-import React, { useState } from 'react'
-import Login from '../components/login/Login';
-
+import styled from "@emotion/styled";
+import { LoginBackGroundImg } from "../assets/login";
+import LoginDetail from "../components/login/LoginDetail";
+import { useNavigate } from "react-router";
+import Button from "../components/common/Button";
 
 function LoginPage() {
-  const [passwordType, setPasswordType] = useState<string>("password");
-
-  const text = [
-    {title: "아이디", name: "id", placeholder: "아이디를 입력해 주세요.", type: "text"},
-    {title: "비밀번호", name: "password", placeholder: "비밀번호를 입력해 주세요.", type: "password"}
-  ]
+  const naigate = useNavigate();
 
   return (
-    <div>
-      <Login setPasswordType={setPasswordType}/>
-    </div>
-  )
+    <Container>
+      <Wrapper>
+        <p>로그인</p>
+        <LoginDetail />
+        <Button text="로그인" />
+        <span>
+          계정이 없으신가요?
+          <GoSignUp onClick={() => naigate("/signup")}>회원가입하기</GoSignUp>
+        </span>
+      </Wrapper>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: linear-gradient(
+      1.82deg,
+      rgba(0, 0, 0, 0.75) 58.5%,
+      rgba(0, 0, 0, 0) 133.9%
+    ),
+    url(${LoginBackGroundImg});
+  background-size: cover;
+  background-position: center;
+  box-shadow: 0px 4px 100px rgba(0, 0, 0, 0.25);
+`;
+
+const Wrapper = styled.div`
+  width: 522px;
+  height: 499px;
+  left: 699px;
+  top: 291px;
+  background-color: ${({ theme }) => theme.WHITE};
+  box-shadow: 0px 4px 100px #000000;
+  border-radius: 8px;
+  padding: 0 70px;
+  box-sizing: border-box;
+  p {
+    display: flex;
+    justify-content: center;
+    font-size: 32px;
+    left: 219px;
+    padding-top: 43px;
+    font-weight: 600;
+  }
+  span {
+    font-size: 12px;
+    display: flex;
+    justify-content: center;
+    line-height: 14px;
+  }
+`;
+
+const GoSignUp = styled.span`
+  font-weight: 600;
+  padding-left: 3px;
+`;
 
 export default LoginPage;
