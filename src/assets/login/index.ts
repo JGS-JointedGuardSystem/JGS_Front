df19ce1cc65571e03ac910ced30925e32dc58c04
inung1004 <dksdbswl10430@naver.com>
2023-05-23 22:21:38 +0900

    🌱 :: (#9) 이미지 추가

--- src/assets/login/index.ts
+++ src/assets/login/index.ts
@@ -1,1 +1,3 @@
-export { default as LoginBackGroundImg } from './LoginBackGroundImg.png';
+export { default as LoginBackGroundImg } from "./imgs/LoginBackGroundImg.png";
+export { default as OnEye } from "./imgs/onEye.svg";
+export { default as OffEye } from "./imgs/offEye.svg";
