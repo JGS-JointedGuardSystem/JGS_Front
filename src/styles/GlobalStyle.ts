dfa3504aca44de6fd57e9ca0406c9120d0bbacb7
inung1004 <dksdbswl10430@naver.com>
2023-05-23 22:25:20 +0900

    👀 :: (#9) 글로벌 스타일 수정

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -2,9 +2,8 @@ import { css } from "@emotion/react";
 
 export const globalStyle = css`
   * {
-    font-family: 'Pretendard', sans-serif !important;
+    font-family: "Pretendard", sans-serif !important;
     -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
-    user-select: none;
     -webkit-touch-callout: none;
     box-sizing: border-box;
     margin: 0;
