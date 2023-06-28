e4f5ba589fc54ef409ee32960080279d5ee976d3
inung1004 <dksdbswl10430@naver.com>
2023-06-28 22:40:07 +0900

    📌 :: (#16) 로그인, 회원가입 코드 수정

--- src/utils/api/auth/index.ts
+++ src/utils/api/auth/index.ts
@@ -3,26 +3,50 @@ import { SignupInputType } from "../../../models/Signup";
 import instance from "../../axios";
 import { LoginInputType } from "../../../models/Login";
 import { setToken } from "../../functions/TokenManager";
+import { useNavigate } from "react-router";
+import { useMutation } from "react-query";
+import { AxiosError } from "axios";
 
-export const userLogin = async (inputsData: LoginInputType) => {
-  return await instance
-    .post(`/login`, {
-      id: inputsData.id,
-      pw: inputsData.password,
-    })
-    .then((res) => {
-      setToken(res.data.accessToken, res.data.refreshToken);
-    });
+export const UserLogin = () => {
+  const navigate = useNavigate();
+
+  return useMutation(
+    async (inputsData: LoginInputType) =>
+      instance.post("/login", {
+        id: inputsData.id,
+        pw: inputsData.password,
+      }),
+    {
+      onError: (error: AxiosError) => {
+        toast.error(`${error.response?.data}`, { duration: 1000 });
+      },
+      onSuccess: ({ data }) => {
+        toast.success("로그인에 성공했습니다.", { duration: 1000 });
+        setToken(data.access_token, data.refresh_token);
+        navigate("/main");
+      },
+    }
+  );
 };
 
-export const userSignup = async (inputsData: SignupInputType) => {
-  if (inputsData.password !== inputsData.checkPassword) {
-    toast.error("비밀번호가 일치하지 않습니다.", { duration: 1000 });
-  } else {
-    await instance.post(`/sign`, {
-      id: inputsData.id,
-      pw: inputsData.password,
-      confirm_code: inputsData.certificationNumber,
-    });
-  }
+export const UserSignup = () => {
+  const navigate = useNavigate();
+
+  return useMutation(
+    async (inputsData: SignupInputType) =>
+      instance.post("/sign", {
+        id: inputsData.id,
+        pw: inputsData.password,
+        confirm_code: inputsData.certificationNumber,
+      }),
+    {
+      onError: (error: AxiosError) => {
+        toast.error(`${error.response?.data}`, { duration: 1000 });
+      },
+      onSuccess: () => {
+        toast.success("회원가입에 성공하였습니다.", { duration: 1000 });
+        navigate("/login");
+      },
+    }
+  );
 };
