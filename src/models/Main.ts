14a3e8da117387a69ca149c79e6b2e8aea173c49
kimjuwon <juwon2298@naver.com>
2023-09-20 00:51:25 +0900

    📝 :: 장치 관련 API 세팅

--- src/models/Main.ts
+++ src/models/Main.ts
@@ -1,7 +1,21 @@
 export interface AddDeviceRequestType {
-  name: string; //장치 이름
-  device_no: number; //장치 고유 번호
-  device_type: string; //장치 타입
-  latitude: number; //위도
-  longitude: number; //경도
+  name: string; // 장치 이름
+  device_no: number; // 장치 고유 번호
+  device_type: string; // 장치 타입
+  latitude: number; // 위도
+  longitude: number; // 경도
+}
+
+export interface RemoveDeviceRequestType {
+  device_no: number;
+}
+
+export interface RenameDeviceRequestType extends RemoveDeviceRequestType {
+  new_name: string; // 새 장치 이름
+}
+
+export interface ChangeDeviceRequestType {
+  device_no: number;
+  latitude: number;
+  longitude: number;
 }
--- src/utils/api/main/index.ts
+++ src/utils/api/main/index.ts
@@ -1,6 +1,23 @@
-import { AddDeviceRequestType } from "../../../models/Main";
+import {
+  AddDeviceRequestType,
+  ChangeDeviceRequestType,
+  RemoveDeviceRequestType,
+  RenameDeviceRequestType,
+} from "../../../models/Main";
 import instance from "../../axios";
 
 export const addDevice = async (request: AddDeviceRequestType) => {
   return await instance.post("/add_device", request);
+};
+
+export const removeDevice = async (request: RemoveDeviceRequestType) => {
+  return await instance.post("/remove_device", request);
+};
+
+export const renameDevice = async (request: RenameDeviceRequestType) => {
+  return await instance.post("/remove_device", request);
+};
+
+export const changeDevice = async (request: ChangeDeviceRequestType) => {
+  return await instance.post("move_device", request);
 };
