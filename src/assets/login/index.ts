2811b811faa71a93a9639901ec369f251b7ed94f
inung1004 <dksdbswl10430@naver.com>
2023-09-20 01:28:40 +0900

    👀 :: 자잘한 수정

--- src/assets/login/index.ts
+++ src/assets/login/index.ts
@@ -1,4 +1,2 @@
 export { default as LoginBackGroundImg } from "./imgs/LoginBackGroundImg.png";
 export { default as Eye } from "./Eye";
-export { default as JGSLogo } from "../icons/Component 6.png";
-export { default as HeadVector } from "../icons/Vector.png";
