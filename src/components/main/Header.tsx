5e0cc28f745b9396b15cecb842f0be3ce35a9119
inung1004 <dksdbswl10430@naver.com>
2023-09-19 14:20:14 +0900

    📌 ::  Header 컴포넌트 분리

---
+++ src/components/main/Header.tsx
@@ -0,0 +1,55 @@
+import styled from "@emotion/styled";
+import { HeadVector, JGSLogo } from "../../assets/login";
+import { useState } from "react";
+import LogOutModal from "../auth/LogOutModal";
+import SmallButton from "../common/SmallButton";
+import { useTheme } from "@emotion/react";
+
+const Header = () => {
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const theme = useTheme();
+
+  return (
+    <>
+      <Container>
+        <LogoImg src={JGSLogo} alt="logo" />
+        <Aside>
+          <Bell src={HeadVector} alt="vector" />
+          <SmallButton
+            text="로그아웃"
+            color={theme.color.BLACK}
+            onClick={() => setIsActive(true)}
+          />
+        </Aside>
+      </Container>
+      {isActive && <LogOutModal setIsActive={setIsActive} />}
+    </>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100vw;
+  height: 65px;
+  padding: 0 10vw;
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.1);
+`;
+
+const LogoImg = styled.img`
+  width: 100px;
+`;
+
+const Aside = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  gap: 30px;
+`;
+
+const Bell = styled.img`
+  width: 20px;
+`;
+
+export default Header;
