18c94ae8abe84822be824731cb8b784af2544b22
inung1004 <dksdbswl10430@naver.com>
2023-09-20 01:30:15 +0900

    📝 :: Error Modal 컴포넌트 추가

---
+++ src/components/main/ErrorModal.tsx
@@ -0,0 +1,70 @@
+import styled from "@emotion/styled";
+import Input from "../common/Input";
+import { useState } from "react";
+import SmallButton from "../common/SmallButton";
+interface LogOutModalProps {
+  setIsActive: React.Dispatch<React.SetStateAction<boolean>>;
+  deviceName: string;
+}
+
+export const ErrorModal = ({ setIsActive, deviceName }: LogOutModalProps) => {
+  const [password, setPassword] = useState<string>("");
+
+  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <Background onClick={() => setIsActive(false)}>
+      <Container onClick={(e) => e.stopPropagation()}>
+        <Text>📣 {deviceName}이 밟혔습니다.</Text>
+        <Input
+          id="password"
+          type="password"
+          placeholder="비밀번호 입력해주세요."
+          onChange={(e) => onChangeInput(e)}
+        />
+        <SmallButton
+          text="확인"
+          onClick={() => setIsActive(false)}
+          color="red"
+        />
+        {/* 비밀번호 확인 API 연동 */}
+      </Container>
+    </Background>
+  );
+};
+
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(255, 3, 3, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 99;
+`;
+
+const Container = styled.div`
+  width: 500px;
+  padding: 40px;
+  height: 260px;
+  border-radius: 16px;
+  background: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+`;
+
+const Text = styled.p`
+  font-size: 25px;
+  line-height: 30px;
+  color: ${({ theme }) => theme.color.BLACK};
+`;
+
+export default ErrorModal;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,8 +1,12 @@
 import Map from "../components/main/Map";
 import Header from "../components/main/Header";
 import styled from "@emotion/styled";
+import { useState } from "react";
+import ErrorModal from "../components/main/ErrorModal";
 
 const MainPage = () => {
+  const [isActive, setIsActive] = useState<boolean>(false);
+
   return (
     <>
       <Header />
@@ -10,6 +14,7 @@ const MainPage = () => {
         <Map />
         <Position>.</Position>
       </MenuWrapper>
+      {isActive && <ErrorModal setIsActive={setIsActive} deviceName="김현민" />}
     </>
   );
 };
