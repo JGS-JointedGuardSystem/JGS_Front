4b658fdc68fbf6b9e979d448652352c7aaa11abe
inung1004 <dksdbswl10430@naver.com>
2023-09-21 00:43:14 +0900

    🌱 :: 수정 아이콘 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as JGSLogo } from "./logo.png";
 export { default as DeleteIcon } from "./trash.svg";
+export { default as RenameIcon } from "./tag.svg";
+export { default as ChangeLocationIcon } from "./location.svg";
