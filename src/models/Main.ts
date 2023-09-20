ee91b31f23cfee900f71267319993d2f5e295d0c
kimjuwon <juwon2298@naver.com>
2023-09-20 22:09:44 +0900

    👀 :: 타입 추가

--- src/models/Main.ts
+++ src/models/Main.ts
@@ -2,8 +2,8 @@ export interface AddDeviceRequestType {
   name: string; // 장치 이름
   device_no: number; // 장치 고유 번호
   device_type: string; // 장치 타입
-  latitude: number; // 위도
-  longitude: number; // 경도
+  latitude?: number; // 위도
+  longitude?: number; // 경도
 }
 
 export interface RemoveDeviceRequestType {
@@ -18,4 +18,10 @@ export interface ChangeDeviceRequestType {
   device_no: number;
   latitude: number;
   longitude: number;
+}
+
+export interface AddDeviceType {
+  title: string;
+  name: string;
+  placeholder: string;
 }
