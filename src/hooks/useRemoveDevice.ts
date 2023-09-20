1d5983a3fda08e8578be80c6ccffec349fc6afd9
inung1004 <dksdbswl10430@naver.com>
2023-09-20 18:44:15 +0900

    📝 :: 장치 제거 API 연동

---
+++ src/hooks/useRemoveDevice.ts
@@ -0,0 +1,23 @@
+import { useMutation } from "react-query";
+import { AxiosError } from "axios";
+import { toast } from "react-hot-toast";
+import { removeDevice } from "../utils/api/main";
+
+interface UseRemoveDeviceProps {
+  device_no: number;
+  deviceName: string;
+}
+
+export const useRemoveDevice = ({
+  device_no,
+  deviceName,
+}: UseRemoveDeviceProps) => {
+  return useMutation(() => removeDevice(device_no), {
+    onError: (error: AxiosError) => {
+      toast.error(`${error.response?.data}`, { duration: 1000 });
+    },
+    onSuccess: () => {
+      toast.success(`${deviceName}가 삭제 되었습니다.`, { duration: 1000 });
+    },
+  });
+};
