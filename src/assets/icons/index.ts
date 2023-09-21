5df2f0dba8868509ec0a0d2c84826b86285d3cba
kimjuwon <juwon2298@naver.com>
2023-09-21 14:31:49 +0900

    👀 :: 오류 해결을 위한 자잘한 수정
    LOGO 이름 변경

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,4 +1,4 @@
-export { default as JGSLogo } from "./logo.png";
+export { default as JGSLOGO } from "./Logo.png";
 export { default as DeleteIcon } from "./trash.svg";
 export { default as RenameIcon } from "./tag.svg";
 export { default as ChangeLocationIcon } from "./location.svg";
