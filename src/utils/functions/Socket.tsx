28fd28186b026061c8d9f71fa946b1a36af9b29a
kimjuwon <juwon2298@naver.com>
2023-09-22 08:30:12 +0900

    👀 :: 임시 Socket 비활성화

--- src/utils/functions/Socket.tsx
+++ src/utils/functions/Socket.tsx
@@ -1,29 +1,37 @@
-import { useEffect } from "react";
-import { io } from "socket.io-client";
-import { getToken } from "./TokenManager";
+// import { useEffect } from "react";
+// import { io } from "socket.io-client";
+// import { getToken } from "./TokenManager";
 
-const BASE_URL = process.env.REACT_APP_PUBLIC_JGS_BASE_URL;
-const NAMESPACE = `/frontend`;
+// const BASE_URL = process.env.REACT_APP_PUBLIC_JGS_BASE_URL;
+// const NAMESPACE = `/frontend`;
 
-export const Connectsocket = io(`${BASE_URL}${NAMESPACE}`, {
-  autoConnect: false,
-});
+// export const Connectsocket = io(`${BASE_URL}${NAMESPACE}`, {
+//   autoConnect: false,
+// });
 
-const { accessToken } = getToken();
+// const { accessToken } = getToken();
 
-const Socket = () => {
-  useEffect(() => {
-    Connectsocket.connect();
+// const Socket = () => {
+//   useEffect(() => {
+//     Connectsocket.connect();
 
-    Connectsocket.emit(
-      "request_data_all",
-      { accesstoken: accessToken },
-      (data: string) => {
-        console.log(data);
-      }
-    );
-  }, []);
-  return <></>;
-};
+//     Connectsocket.emit(
+//       "request_data_all",
+//       { accesstoken: accessToken },
+//       (data: string) => {
+//         console.log(data);
+//       }
+//     );
+//   }, []);
+//   return <></>;
+// };
+
+// export default Socket;
+
+import React from "react";
+
+function Socket() {
+  return <div>Socket</div>;
+}
 
 export default Socket;
