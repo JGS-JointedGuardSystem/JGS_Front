4d8db3a0681b55686d77dbb1b24959c6c4336ebe
kimjuwon <juwon2298@naver.com>
2023-09-24 21:38:41 +0900

    👀 :: toast 수정

--- src/hooks/useChangeDeviceName.ts
+++ src/hooks/useChangeDeviceName.ts
@@ -6,14 +6,14 @@ import { RenameDeviceRequestType } from "../models/Main";
 
 export const useChangeDeviceName = ({
   device_no,
-  Device_new_name,
+  device_new_name,
 }: RenameDeviceRequestType) => {
-  return useMutation(() => renameDevice({ device_no, Device_new_name }), {
+  return useMutation(() => renameDevice({ device_no, device_new_name }), {
     onError: (error: AxiosError) => {
       toast.error(`${error.response?.data}`, { duration: 1000 });
     },
     onSuccess: () => {
-      toast.success(`장치 이름이 ${Device_new_name}로 변경 되었습니다.`, {
+      toast.success(`장치 이름이 ${device_new_name}(으)로 변경 되었습니다.`, {
         duration: 1000,
       });
     },
