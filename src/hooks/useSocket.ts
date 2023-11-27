eca55cf58c2968fdc2ce9ed92b6fd06f414ec4cd
juwon <juwon2298@naver.com>
2023-11-27 14:43:16 +0900

    socket

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,56 +1,39 @@
 import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
-import { getToken } from "../utils/functions/TokenManager";
-// import { DeviceType } from "../models/Main";
+import { getToken } from "../utils/functions/TokenManager"
+import { io } from "socket.io-client"
 
 const BASE_URL = process.env.REACT_APP_PUBLIC_JGS_BASE_URL;
 
-type StatusType = "loading" | "connected" | "disconnected";
+export type statusType = 'loading' | 'connected' | 'disconnected';
 
 export const useSocket = () => {
-  const [status, setStatus] = useState<StatusType>("disconnected");
-  const [data, setData] = useState([]);
-  const { accessToken } = getToken();
+  const [status, setStatus] = useState<statusType>('disconnected');
 
-  const Connectsocket = io(`${BASE_URL}/frontend`, {
-    autoConnect: false,
-    forceNew: true,
-    transports: ["websocket"],
-  });
-
-  const EmbbedSocket = io(`${BASE_URL}`, {
+  const socket = io(`${BASE_URL}/frontend`, {
     autoConnect: false,
     forceNew: true,
     transports: ["websocket"],
   });
 
   useEffect(() => {
-    if (status === "loading" && data.length > 0) {
-      setStatus("connected");
-    }
-  }, [data]);
+    return () => disconnect();
+  }, [])
 
-  const Connect = () => {
+  const connect = () => {
+    const { accessToken } = getToken();
     if (status === "disconnected") {
       setStatus("loading");
-      Connectsocket.connect();
-      Connectsocket.emit("request_data_all", { accesstoken: accessToken });
+      socket.connect();
+      socket.emit("request_data_all", {
+        accesstoken: accessToken,
+      });
     }
-  };
+  }
 
-  const Disconnect = () => {
-    if (status === "connected") {
-      Connectsocket.disconnect();
-      EmbbedSocket.disconnect();
-      setStatus("disconnected");
-      setData([]);
-    }
-  };
-  return { data, status, Connect, Disconnect };
-};
+  const disconnect = () => {
+    socket.disconnect();
+    setStatus('disconnected');
+  }
 
-// EmbbedSocket.on("Alert", ({ name, id, lat, long, type }) => {
-//   alert(
-//     `Name: ${name}, ID: ${id}, Latitude: ${lat}, Longitude: ${long}, Type: ${type}`
-//   );
-// });
+  return { status, connect, disconnect };
+}
